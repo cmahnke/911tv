f50eb3abf13e304e9943252d92b00488425286d0
Christian Mahnke <cmahnke@gmail.com>
2024-10-10 00:14:39 +0200

    Make sure we fail on error

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-./scripts/image-assets.sh 
+set -e
+
+./scripts/image-assets.sh
 
 python ./scripts/gen_urls.py > ./site/src/assets/json/urls.json
 python ./scripts/gen_pages.py > ./site/src/assets/json/pages.json
