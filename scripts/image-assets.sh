df95390463c11a084e06e5558080b83bc07db66f
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-05-11 09:51:15 +0200

    Use only magick

--- scripts/image-assets.sh
+++ scripts/image-assets.sh
@@ -4,21 +4,21 @@ set -e
 
 mkdir -p site/build
 
-magick convert site/public/images/screenshot.jpg -fuzz 5% -fill none -draw 'color 1,1 floodfill' -trim +repage site/public/images/favicon.png
+magick site/public/images/screenshot.jpg -fuzz 5% -fill none -draw 'color 1,1 floodfill' -trim +repage site/public/images/favicon.png
 
 #-transparent white
-magick convert site/public/images/favicon.png -resize 512x512  site/public/images/favicon-512.png
-magick convert site/public/images/favicon-512.png -resize 256x256 -background transparent -gravity center -extent 256x256 site/public/images/favicon-256.png
-magick convert site/public/images/favicon-256.png -resize 128x128 -background transparent -gravity center -extent 128x128 site/public/images/favicon-128.png
-magick convert site/public/images/favicon-256.png -resize 64x64 -background transparent -gravity center -extent 64x64 site/public/images/favicon-64.png
-magick convert site/public/images/favicon-256.png -resize 32x32 -background transparent -gravity center -extent 32x32 site/public/images/favicon-32.png
-magick convert site/public/images/favicon-256.png -resize 16x16 -background transparent -gravity center -extent 16x16 site/public/images/favicon-16.png
-magick convert site/public/images/favicon-16.png site/public/images/favicon-32.png site/public/images/favicon-64.png site/public/images/favicon-128.png -colors 256 site/public/images/favicon.ico
-magick convert site/public/images/favicon-16.png site/public/images/favicon-32.png site/public/images/favicon-64.png site/public/images/favicon-128.png site/public/images/favicon-256.png -colors 256 site/build/icon.ico
+magick site/public/images/favicon.png -resize 512x512  site/public/images/favicon-512.png
+magick site/public/images/favicon-512.png -resize 256x256 -background transparent -gravity center -extent 256x256 site/public/images/favicon-256.png
+magick site/public/images/favicon-256.png -resize 128x128 -background transparent -gravity center -extent 128x128 site/public/images/favicon-128.png
+magick site/public/images/favicon-256.png -resize 64x64 -background transparent -gravity center -extent 64x64 site/public/images/favicon-64.png
+magick site/public/images/favicon-256.png -resize 32x32 -background transparent -gravity center -extent 32x32 site/public/images/favicon-32.png
+magick site/public/images/favicon-256.png -resize 16x16 -background transparent -gravity center -extent 16x16 site/public/images/favicon-16.png
+magick site/public/images/favicon-16.png site/public/images/favicon-32.png site/public/images/favicon-64.png site/public/images/favicon-128.png -colors 256 site/public/images/favicon.ico
+magick site/public/images/favicon-16.png site/public/images/favicon-32.png site/public/images/favicon-64.png site/public/images/favicon-128.png site/public/images/favicon-256.png -colors 256 site/build/icon.ico
 
 # These doesn't allow as much controll as needed
-#magick convert site/public/images/favicon.png -define icon:auto-resize=64,48,32,16 site/public/images/favicon.ico
-#magick convert site/public/images/favicon.png -define icon:auto-resize=256,128,64,48,32,16 site/build/icon.ico
+#magick site/public/images/favicon.png -define icon:auto-resize=64,48,32,16 site/public/images/favicon.ico
+#magick site/public/images/favicon.png -define icon:auto-resize=256,128,64,48,32,16 site/build/icon.ico
 
 
 if ! command -v icnsify 2>&1 >/dev/null ; then
